2d71ba4a1e7dfbb37df1aa2d74ce6faa7b87e9df
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-05-08 20:26:21 -0300

    Install external librarys

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from "react-native"
+import { StatusBar } from "react-native";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "./components/ui/gluestack-ui-provider/config";
 import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-    <GluestackUIProvider config={config}>
-      <StatusBar 
+    <GluestackUIProvider config={config} colorMode="light">
+      <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Center } from "@/components/ui/center";
+import { Text } from "@/components/ui/text";
 
 export default function Home() {
   return (
-    <View>
+    <Center className="flex-1">
       <Text>Aqui tem corage</Text>
-    </View>
+    </Center>
   );
 }
