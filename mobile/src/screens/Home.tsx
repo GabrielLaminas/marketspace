aac8d507b6d90df500237856423baf624013649d
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-05-10 11:39:40 -0300

    Confit nativewind

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -4,10 +4,8 @@ import { Text } from "@/components/ui/text";
 
 export default function Home() {
   return (
-    <Center className="size-full flex-1 border border-red-600 bg-red-200">
-      <Text className="text-typography-0 font-bold">
-        This is the center LIXOOO.
-      </Text>
+    <Center className="flex-1 bg-red-200">
+      <Text className="text-typography-0 font-bold">My App</Text>
     </Center>
   );
 }
