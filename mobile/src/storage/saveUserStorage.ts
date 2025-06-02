b2a0232b28ff94e99a0cb2fc94f0781da0432a93
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-06-02 19:55:14 -0300

    update save user storage

--- mobile/src/storage/saveUserStorage.ts
+++ mobile/src/storage/saveUserStorage.ts
@@ -1,11 +1,11 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { UserSignInDTO } from "@dtos/User";
+import { UserDTO } from "@dtos/User";
 import { COLLECTION_USER } from "./configStorage";
 
-export default async function saveUserStorage(data: UserSignInDTO){
+export default async function saveUserStorage(user: UserDTO){
   try {
-    await AsyncStorage.setItem(COLLECTION_USER, JSON.stringify(data));    
+    await AsyncStorage.setItem(COLLECTION_USER, JSON.stringify(user));    
   } catch (error) {
     throw error;
   }
