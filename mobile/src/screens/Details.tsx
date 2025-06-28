57ea52ee26f2c23f4ae0ae2e5030493342e816f8
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-06-28 13:19:59 -0300

    Finish page details

--- mobile/src/screens/Details.tsx
+++ mobile/src/screens/Details.tsx
@@ -1,8 +1,13 @@
-import { ScrollView } from "react-native";
-import React from "react";
+import { Alert, Linking, ScrollView } from "react-native";
+import { useCallback, useState } from "react";
 
-import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
-import { AppRoutesProps } from "@routes/app.routes";
+import { useFocusEffect, useNavigation, useRoute } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+
+import api from "@services/api";
+
+import { ImagesPickerProps, PaymentMethods, ProductData } from "@dtos/Product";
+import PaymentMethodsList from "@components/PaymentMethodsList";
 
 import { Box } from "@/components/ui/box";
 import { Text } from "@/components/ui/text";
@@ -15,22 +20,69 @@ import { Image } from "@/components/ui/image";
 import CustomButton from "@components/CustomButton";
 import Carousel from "@components/Carousel";
 import Header from "@components/Header";
+import Loading from "@components/Loading";
 
-import { Barcode, Bank, Money, QrCode, CreditCard } from "phosphor-react-native";
 import { ArrowLeft, Phone } from "lucide-react-native";
 
-const DATA = [
-  "https://cdn.awsli.com.br/600x700/1259/1259538/produto/238747959/img_6666-8zgzqibzh0.jpg",
-  "https://down-br.img.susercontent.com/file/sg-11134201-7rd57-lwyqk6femsfa96",
-  "https://lebiscuit.vtexassets.com/arquivos/ids/21689609/17302131428199.jpg?v=638679144080530000"
-]
+type ImagesProps = ImagesPickerProps & {
+  id?: string;
+};
 
-type Props = BottomTabScreenProps<AppRoutesProps, "Details">;
+export default function Details() {
+  const [images, setImages] = useState<ImagesProps[]>([]);
+  const [product, setProduct] = useState<ProductData>({} as ProductData);
+  const [loading, setLoading] = useState(false);
 
-export default function Details({ navigation }: Props) {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const route = useRoute();
+  const params = route.params as { id: string };
 
   function handleNavigationToGoBack(){
     navigation.goBack();
+  }
+
+  async function getProductById() {
+    try {
+      setLoading(true);
+      const { data } = await api.get<ProductData>(`/products/${params.id}`);
+
+      const image: ImagesProps[] = data.product_images.map((image) => {
+        return {
+          id: image.id,
+          name: image.path,
+          uri: `${api.defaults.baseURL}/images/${image.path}`,
+          type: `image/${image.path.split(".")[1]}`,
+        };
+      });
+
+      setImages(image);
+      setProduct(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const handleSendWhatsappMessage = useCallback(async () => {
+    const url = `https://wa.me/${product.user?.tel}`;
+    const suportLink = await Linking.canOpenURL(url);
+
+    if(suportLink){
+      await Linking.openURL(url);
+    } else {
+      Alert.alert("Anúncio", "Não é possível abrir o link!");
+    }
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      getProductById();
+    }, [params.id]),
+  );
+
+  if(loading){
+    <Loading />
   }
 
   return (
@@ -44,90 +96,72 @@ export default function Details({ navigation }: Props) {
         </Box>
 
         <Carousel 
-          data={DATA}
+          data={images}
         />
 
         <VStack space="2xl" className="px-[24px] pt-[22px] pb-[26px]">
           <HStack space="sm" className="items-center">
             <Image 
-              source={{ uri: "https://mighty.tools/mockmind-api/content/human/104.jpg" }}
+              source={{ 
+                uri: `${api.defaults.baseURL}/images/${product.user?.avatar}` 
+              }}
               size="none"
               width={28}
               height={28}
               alt="nome qualquer"
               className="rounded-full border-2 border-product-secundary bg-slate-400"
             />
-            <Text className="text-base text-base-100">Makenna Baptista</Text>
+            <Text className="text-base text-base-100">{product.user?.name}</Text>
           </HStack>
 
           <VStack space="sm" className="items-start">
             <Badge className="px-[8px] py-[2px] bg-base-500 rounded-full">
-              <BadgeText className="uppercase font-heading text-center text-base-200">Novo</BadgeText>
+              <BadgeText className="uppercase font-heading text-center text-base-200">{product.is_new ? "Novo" : "Usado"}</BadgeText>
             </Badge>
 
             <HStack className="w-full justify-between items-center">
-              <Heading className="text-base-100 text-2xl">Bicicleta</Heading>
+              <Heading className="text-base-100 text-2xl">{product.name}</Heading>
 
               <Heading className="text-product-secundary text-2xl">
                 <Text className="text-product-secundary text-base mr-2">R$</Text>
-                120,00
+                {product.price}
               </Heading>
             </HStack>
 
-            <Text className="w-full text-base text-base-200">
-              Cras congue cursus in tortor sagittis placerat nunc, tellus arcu. Vitae ante leo eget maecenas urna mattis cursus. Mauris metus amet nibh mauris mauris accumsan, euismod. Aenean leo nunc, purus iaculis in aliquam.
-            </Text>
+            <Text className="w-full text-base text-base-200">{product.description}</Text>
           </VStack>
 
           <HStack space="md" className="items-center">
             <Heading className="text-base-200 text-base">Aceita troca?</Heading>
-            <Text className="text-base-200 text-base">Sim</Text>
+            <Text className="text-base-200 text-base">{product.accept_trade ? "Sim" : "Não"}</Text>
           </HStack>
 
           <VStack space="md">
             <Heading className="text-base-200 text-base">Meios de pagamento:</Heading>
 
-            <VStack space="md">
-              <HStack space="sm" className="items-center">
-                <Barcode size={20} color="#1A181B" />
-                <Text className="text-base-200 text-base">Boleto</Text>
-              </HStack>
-
-              <HStack space="sm" className="items-center">
-                <QrCode size={20} color="#1A181B" />
-                <Text className="text-base-200 text-base">Pix</Text>
-              </HStack>
-
-              <HStack space="sm" className="items-center">
-                <Money size={20} color="#1A181B" />
-                <Text className="text-base-200 text-base">Dinheiro</Text>
-              </HStack>
-
-              <HStack space="sm" className="items-center">
-                <CreditCard size={20} color="#1A181B" />
-                <Text className="text-base-200 text-base">Cartão de Crédito</Text>
-              </HStack>
-
-              <HStack space="sm" className="items-center">
-                <Bank size={20} color="#1A181B" />
-                <Text className="text-base-200 text-base">Depósito Bancário</Text>
-              </HStack>
-            </VStack>
+            <PaymentMethodsList
+              paymentsMethods={
+                product.payment_methods?.map(
+                  (method) => method.key,
+                ) as PaymentMethods[]
+              }
+            />
           </VStack>
         </VStack>
       </ScrollView>
 
-      <Box className="px-[24px] pt-[20px] pb-[28px] bg-base-700">
+      <Box className="px-[24px] pt-[20px] pb-[28px] bg-white">
         <HStack space="md" className="justify-between items-center">
           <Heading className="text-product-primary text-3xl">
             <Text className="text-product-primary text-lg mr-2">R$</Text>
-            120,00
+            {product.price}
           </Heading>
 
           <CustomButton
             text="Entrar em contato"
             variant="PRIMARY"
             icon={Phone}
+            onPress={handleSendWhatsappMessage}
           />
         </HStack>
       </Box>
